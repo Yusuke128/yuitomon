b70282b8ecd3cd53622a85ce0fb9538110777cb9
Yusuke128 <bass.kyusuke@gmail.com>
2025-12-28 02:55:21 +0900

    footer,kv,pickup_scss-forPC

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+$(".pickup-slider").slick({
+  autoplay: false,
+  dots: false,
+  arrows: true,
+  centerMode: true,
+  variableWidth: true,
+  infinite: true,
+  prevArrow:
+    '<button class="slick-prev pickup-arrow" aria-label="Previous"><span class="material-symbols-outlined">keyboard_arrow_left</span></button>',
+  nextArrow:
+    '<button class="slick-next pickup-arrow" aria-label="next"><span class="material-symbols-outlined">keyboard_arrow_right</span></button>'
+});
