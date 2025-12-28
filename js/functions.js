53e9e76590fd013b6b2a3d61e07529b5e5104664
Yusuke128 <bass.kyusuke@gmail.com>
2025-12-29 03:31:06 +0900

    responsive_header

--- js/functions.js
+++ js/functions.js
@@ -8,5 +8,31 @@ $(".pickup-slider").slick({
   prevArrow:
     '<button class="slick-prev pickup-arrow" aria-label="Previous"><span class="material-symbols-outlined">keyboard_arrow_left</span></button>',
   nextArrow:
-    '<button class="slick-next pickup-arrow" aria-label="next"><span class="material-symbols-outlined">keyboard_arrow_right</span></button>'
+    '<button class="slick-next pickup-arrow" aria-label="next"><span class="material-symbols-outlined">keyboard_arrow_right</span></button>',
+  responsive: [
+    {
+      breakpoint: 796,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+//ハンバーガーメニュー
+const hum = jQuery(".humberger");
+const header = jQuery(".header");
+const body = jQuery("body");
+
+hum.on("click", function () {
+  header.toggleClass("open");
+  body.toggleClass("no-scroll");
+});
+
+// SP版でナビリンククリック時にメニューを閉じる
+jQuery(".link").on("click", function () {
+  if (header.hasClass("open")) {
+    header.removeClass("open");
+    body.removeClass("no-scroll");
+  }
 });
