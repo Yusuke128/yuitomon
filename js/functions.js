0030bc54ada0153c95abff1459bda8b5a818dc14
Yusuke128 <bass.kyusuke@gmail.com>
2026-01-02 02:24:08 +0900

    slider_change-swiper

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,39 @@
-$(".pickup-slider").slick({
-  autoplay: true,
-  dots: false,
-  arrows: true,
-  slideToShow: 3,
-  centerMode: true,
-  centerPadding: "25%",
-  prevArrow:
-    '<button class="slick-prev pickup-arrow" aria-label="Previous"><span class="material-symbols-outlined">keyboard_arrow_left</span></button>',
-  nextArrow:
-    '<button class="slick-next pickup-arrow" aria-label="next"><span class="material-symbols-outlined">keyboard_arrow_right</span></button>',
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slideToShow: 1,
-        centerMode: false
-      }
-    }
-  ]
+// $(".pickup-slider").slick({
+//   autoplay: true,
+//   dots: false,
+//   arrows: true,
+//   slideToShow: 3,
+//   centerMode: true,
+//   centerPadding: "25%",
+//   prevArrow:
+//     '<button class="slick-prev pickup-arrow" aria-label="Previous"><span class="material-symbols-outlined">keyboard_arrow_left</span></button>',
+//   nextArrow:
+//     '<button class="slick-next pickup-arrow" aria-label="next"><span class="material-symbols-outlined">keyboard_arrow_right</span></button>',
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         slideToShow: 1,
+//         centerMode: false
+//       }
+//     }
+//   ]
+// });
+const pickup_slider = new Swiper(".pickup-swiper", {
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 28,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  autoplay: {
+    delay: 4000,
+    speed: 600,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true
+  }
 });
 
 //ハンバーガーメニュー
