c194da15a02c08a2d3b11d009f6d3c1e3a13236e
Yusuke128 <bass.kyusuke@gmail.com>
2026-01-06 02:47:50 +0900

    problems_edit

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,3 @@
-// $(".pickup-slider").slick({
-//   autoplay: true,
-//   dots: false,
-//   arrows: true,
-//   slideToShow: 3,
-//   centerMode: true,
-//   centerPadding: "25%",
-//   prevArrow:
-//     '<button class="slick-prev pickup-arrow" aria-label="Previous"><span class="material-symbols-outlined">keyboard_arrow_left</span></button>',
-//   nextArrow:
-//     '<button class="slick-next pickup-arrow" aria-label="next"><span class="material-symbols-outlined">keyboard_arrow_right</span></button>',
-//   responsive: [
-//     {
-//       breakpoint: 768,
-//       settings: {
-//         slideToShow: 1,
-//         centerMode: false
-//       }
-//     }
-//   ]
-// });
 const pickup_slider = new Swiper(".pickup-swiper", {
   loop: true,
   centeredSlides: true,
@@ -27,13 +6,13 @@ const pickup_slider = new Swiper(".pickup-swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
-  },
-  autoplay: {
-    delay: 4000,
-    speed: 600,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true
   }
+  // autoplay: {
+  //   delay: 4000,
+  //   speed: 600,
+  //   disableOnInteraction: false,
+  //   pauseOnMouseEnter: true
+  // }
 });
 
 //ハンバーガーメニュー
